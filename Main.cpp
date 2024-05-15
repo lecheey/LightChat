62059037d5ff83fe651a064c48adf931875aede5
lecheey <iva@archkek.localdomain>
2024-05-15 19:19:29 +0300

    input fix

--- Main.cpp
+++ Main.cpp
@@ -1,95 +1,60 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include "Chat.h"
 #include "Sha1.h"
-using namespace std;
-
-void userHelp(){
-	cout << "[r]egistration\tдля регистрации нового пользователя\n[l]og in\tдля входа в аккаунт\n[c]atalog\tдля вывода списка пользователей\n[q]uit\tдля выхода из программы\n" << endl;
-}
+#include "Func.h"
 
 int main(){
 	Chat chat;
+	chat.readUser();
 		
-	cout << "---------- Добро пожаловать в чат ----------" << endl;
-	cout << "Для работы с программой используйте:\n";
+	std::cout << "---------- Добро пожаловать в чат ----------" << std::endl;
+	std::cout << "Меню:\n";
 
 	bool running = true;
-	while(running){
-		// переменные для логина и пароля
-		std::string login, pass;
-		char* _l;
-		char* _p;
+	bool logged = false;
 	
+	while(running){
+		std::string _login;
+		char _pass[16];
+
 		userHelp();
-		cout << "Главное меню: ";
-		char choice;
-		cin >> choice; // выбор действий пользователя
-		cin.ignore(256, '\n'); // игнорирование ввода символов после первого
+		std::cout << "Главное меню: ";
 		
-		// опции
+		char choice;
+		std::cin >> choice;
+		std::cin.ignore(256, '\n');
+		
 		switch(choice){
 			case 'r':
 				system("clear");
 				std::cout << "Регистрация нового пользователя\n";
 				std::cout << "Придумайте логин: ";
-				std::getline(std::cin, login);
-				_l = login.data();
+				std::getline(std::cin, _login, '\n');
 				
 				std::cout << "Придумайте пароль: ";
-				std::getline(std::cin, pass);
-				_p = pass.data();
+				std::cin.getline(_pass, 16, '\n');
 				
-				chat.regUser(_l, _p, size(pass));
+				chat.regUser(_login, _pass, std::strlen(_pass));
 				break;
 			case 'l':
 				system("clear");
 				std::cout << "Авторизация пользователя\n";
 				std::cout << "Введите логин: ";
-				std::getline(std::cin, login);
-				_l = login.data();
+				std::getline(std::cin, _login, '\n');
 				
 				std::cout << "Введите пароль: ";
-				std::getline(std::cin, pass);
-				_p = pass.data();
+				std::cin.getline(_pass, 16, '\n');
 
-				bool logged;
-				logged = chat.authUser(_l, _p, size(pass));
-				
-				// отправка сообщений
+				logged = chat.authUser(_login, _pass, std::strlen(_pass));
 				while(logged){
-					// выбор собеседника (чата)	
-					std::cout << "Введите имя собеседника ([q]uit для выхода):\n----------\nall\t(общий диалог)\n";
-					chat.showUsers();
-					
-					std::cout << login << ": "; // имя отправителя
-					std::string to;
-					std::getline(std::cin, to);
-					
-					// отмена
-					if(to == "q"){
-						logged = chat.logOut(_l);
-						break;
-					}
-
-					char* _to;
-					_to = to.data();
-
-					system("clear");
-					if(!strcmp(_to, "all")){
-						std::cout << "---------- Групповой чат ----------" << std::endl;
-					}
-					else if(chat.getIndex(_to) < 0){ continue; }
-					else{
-						std::cout << "---------- Собеседник  " << to << " ----------" << std::endl; 
-					}
-					chat.getMsgs(_l, _to); // история сообщений
-					chat.userTyping(_l, _to); // отправка сообщений
-				}	
+					chat.userRuntime(_login);
+					logged = chat.logOut(_login);
+				}
 				break;
 			case 'c':
 				system("clear");
-				cout << "Список пользователей:\n----------\nлогин\tимя\tстатус\n";
+				cout << "Список пользователей:\n----------\nЛогин\tСтатус\n";
 				chat.showUsers();
 				cout << "----------\n\n";
 				break;
